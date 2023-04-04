9d06dd2ad2826ef2aed9e3d54fbb96e45752b12f
rizkifajarr <rizkifajarsetyawan28@gmail.com>
2023-04-04 22:11:33 +0700

    Bikin Models

---
+++ app/Models/Kehadiran.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kehadiran extends Model
+{
+    use HasFactory;
+    protected $table = 'kehadiran';
+    protected $fillable = [
+        'user_id',
+        'jam_masuk',
+        'jam_keluar',
+        'status'
+    ];
+}
