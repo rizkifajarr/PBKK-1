795d70958de2baace50fb45ac1ef79a41f31a547
rizkifajarr <rizkifajarsetyawan28@gmail.com>
2023-04-04 21:55:32 +0700

    Bikin Tabel Database & Ganti Routes

---
+++ database/migrations/2023_04_04_144112_create_kehadiran_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kehadiran', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->date('jam_masuk');
+            $table->date('jam_keluar');
+            $table->enum('status', ['A', 'M', 'I', 'S']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kehadiran');
+    }
+};
