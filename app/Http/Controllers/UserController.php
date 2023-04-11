8e9b1fbf3d9a214c563634c0746fb32326c0c941
rizkifajarr <rizkifajarsetyawan28@gmail.com>
2023-04-11 22:05:01 +0700

    ...

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
+class UserController extends Controller
+{
+    public function getUser(User $user)
+    {
+        $dataUser = $user->get();
+        return view('viewUser', compact('dataUser'));
+    }
+    public function saveUser()
+    {
+    }
+    public function deleteUser()
+    {
+    }
+    public function updateUser()
+    {
+    }
+}
